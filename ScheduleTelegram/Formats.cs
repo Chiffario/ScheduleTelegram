4c0acc1b172b2ae4d85cac719b80e8bcf9d41384
Chiffa <artemy040404@gmail.com>
2021-12-27 20:30:26 +0300

    slight changes

---
+++ ScheduleTelegram/Formats.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
+
+public class Formats
+{
+    // JSON для изначального форматирования гугл-таблицы
+    public class Lessons
+    {
+        public string MajorDimension { get; set; }
+        public string Range { get; set; }
+        public List<List<string>> Values { get; set; }
+        public object ETag { get; set; }
+    }
+    public class LessonsReformatted
+    {
+        public string ClassOne { get; set; }
+        public string ClassTwo { get; set; }
+        public string ClassThree { get; set; }
+        public string ClassFour { get; set; }
+        public string ClassFive { get; set; }
+        public string ClassSix { get; set; }
+        public string ClassSeven { get; set; }
+        public string ClassEight { get; set; }
+    }
+    public enum SubjNames
+    {
+
+    }
+}
---
+++ ScheduleTelegram/UserSetup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+using System.Linq;
+public class UserSetup
+{
+    public static async Task StatusSetup(ITelegramBotClient botClient, Message message)
+    {
+        ReplyKeyboardMarkup kbMarkup = new(new[]
+        {
+            new KeyboardButton[] { "Учитель", "Ученик" },
+            new KeyboardButton[] { "Классный руководитель", "Ира" },
+        })
+        {
+            OneTimeKeyboard = true,
+            ResizeKeyboard = true
+        };
+
+        Message sentMessage = await botClient.SendTextMessageAsync(
+                    chatId: message.Chat.Id,
+                    text: "Выберите, кто вы",
+                    replyMarkup: kbMarkup);
+    }
+    //public static async Task GradeSetup(ITelegramBotClient botClient, Message message)
+    //{
+    //    Message sentMessage = await botClient.SendTextMessageAsync(
+    //                chatId: message.Chat.Id,
+    //                text: "Напишите свой класс. Если у номера класса есть буква, напишите класс и букву без пробелов, например \"6а\"",
+    //                parseMode: ParseMode.Markdown);
+
+    //    await 
+    //}
+}
